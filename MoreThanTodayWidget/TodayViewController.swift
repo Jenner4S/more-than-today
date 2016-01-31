3b3f381818f62f051ec659b78be0211a36f57763
Assaf Gelber <assaf.gelber@gmail.com>
2016-01-31 15:51:22 +0200

    Implicitly unwrap cells

--- MoreThanTodayWidget/TodayViewController.swift
+++ MoreThanTodayWidget/TodayViewController.swift
@@ -145,13 +145,13 @@ extension TodayViewController: UITableViewDataSource, UITableViewDelegate {
 
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     if events.isEmpty {
-      let cell = tableView.dequeueReusableCellWithIdentifier(String(EmptyStateCell)) as? EmptyStateCell
-      return cell ?? UITableViewCell()
+      let cell = tableView.dequeueReusableCellWithIdentifier(String(EmptyStateCell)) as! EmptyStateCell
+      return cell
     } else {
-      let cell = tableView.dequeueReusableCellWithIdentifier(String(EventCell)) as? EventCell
-      cell?.event = events[indexPath.section][indexPath.row]
-      cell?.timeWidth = longestTime
-      return cell ?? UITableViewCell()
+      let cell = tableView.dequeueReusableCellWithIdentifier(String(EventCell)) as! EventCell
+      cell.event = events[indexPath.section][indexPath.row]
+      cell.timeWidth = longestTime
+      return cell
     }
   }
 
