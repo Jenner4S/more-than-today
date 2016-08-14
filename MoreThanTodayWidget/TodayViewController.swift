79c90d3cd6bf73d390844f911075324be83a862a
Assaf Gelber <assaf.gelber@gmail.com>
2016-08-14 11:26:29 +0300

    Revert "Trim events to max 5"
    This reverts commit ac75774b1124761fd5e1203d088c496a74862236.

--- MoreThanTodayWidget/TodayViewController.swift
+++ MoreThanTodayWidget/TodayViewController.swift
@@ -14,7 +14,6 @@ import Crittercism
 class TodayViewController: UIViewController, NCWidgetProviding {
   private let TOP_INSET_CORRECTION: CGFloat = -6
   private let HEADER_HEIGHT: CGFloat = 36
-  private let MAX_EVENTS = 5
   private let TIME_FONT = UIFont.systemFontOfSize(12)
 
   private let store = EKEventStore()
@@ -101,8 +100,7 @@ extension TodayViewController {
         let endDate = DatesHelper.startOfDayForDaysFromNow(requiredSelf.daysForward)
         let predicate = requiredSelf.store.predicateForEventsWithStartDate(NSDate(), endDate: endDate, calendars: requiredSelf.calendars)
         let ekEvents = requiredSelf.store.eventsMatchingPredicate(predicate)
-        let trimmedEvents = ekEvents.count > requiredSelf.MAX_EVENTS ? Array(ekEvents[0..<requiredSelf.MAX_EVENTS]) : ekEvents
-        let newEvents = requiredSelf.groupEvents(trimmedEvents.map { Event(ekEvent: $0) })
+        let newEvents = requiredSelf.groupEvents(ekEvents.map { Event(ekEvent: $0) })
         updateResult = newEvents != requiredSelf.events ? .NewData : .NoData
         requiredSelf.events = newEvents
         EventCache.cacheEvents(requiredSelf.events)
